aaaaa303b38f25347a8573c136fddc104947e2e5
03055358031 <>
2022-11-09 21:19:36 -0300

    Funcionalidade de exclusão de motorista

---
+++ src/model/dao/MotoristaDAO.java
@@ -0,0 +1,79 @@
+package model.dao;
+import model.bean.Motorista;
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+
+public class MotoristaDAO {
+    
+
+    public void create(Motorista m) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        try{
+            stmt = con.prepareStatement("INSERT INTO motorista (nome, genero, rg, cpf, celular, email, senha) VALUES (?,?,?,?,?,?,?)");
+            stmt.setString(1, m.getNome());
+            stmt.setString(2, m.getGenero());
+            stmt.setString(3, m.getRg());
+            stmt.setString(4, m.getCpf());
+            stmt.setString(5, m.getCelular());
+            stmt.setString(6, m.getEmail());
+            stmt.setString(7, m.getSenha());
+            
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Motorista cadastrado com sucesso");
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro ao salvar: " + e);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+    public List<Motorista> read() {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Motorista> motoristas = new ArrayList<>();
+        try{
+            stmt = con.prepareStatement("SELECT * FROM motorista;");
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                Motorista m = new Motorista();
+                m.setIdMotorista(rs.getInt("idMotorista"));
+                m.setNome(rs.getString("nome"));
+                m.setGenero(rs.getString("genero"));
+                m.setRg(rs.getString("rg"));
+                m.setCpf(rs.getString("cpf"));
+                m.setCelular(rs.getString("celular"));
+                m.setEmail(rs.getString("email"));
+                m.setSenha(rs.getString("senha"));
+                motoristas.add(m);
+            }
+        } catch(SQLException e) {
+            throw new RuntimeException("Erro ao buscar os dados: ", e);
+        } finally{
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        return motoristas;
+    }
+    public void delete(Motorista m)  {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement("DELETE  FROM motorista where idMotorista =?");
+            stmt.setInt(1, m.getIdMotorista());
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Motorista excluído com sucesso");
+        }
+        catch (SQLException e) {
+            throw new RuntimeException("Erro ao deletar motorista: " + e);
+        }
+        finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }    
+    }
+}
---
+++ src/model/dao/VagaDAO.java
@@ -0,0 +1,71 @@
+package model.dao;
+import model.bean.Vaga;
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+
+public class VagaDAO {
+    
+    public void create(Vaga v) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        try{
+            stmt = con.prepareStatement("INSERT INTO vaga (numero, rua, obliqua) VALUES (?,?,?)");
+            stmt.setInt(1, v.getNumero());
+            stmt.setString(2, v.getRua());
+            stmt.setBoolean(3, v.isObliqua());
+            
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Vaga salva com sucesso");
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro ao salvar: " + e);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+    public List<Vaga> read() {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Vaga> vagas = new ArrayList<>();
+        try{
+            stmt = con.prepareStatement("SELECT * FROM vaga;");
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                Vaga v = new Vaga();
+                v.setIdVaga(rs.getInt("idVaga"));
+                v.setNumero(rs.getInt("numero"));
+                v.setRua(rs.getString("rua"));
+                v.setObliqua(rs.getBoolean("obliqua"));
+                vagas.add(v);
+            }
+        } catch(SQLException e) {
+            throw new RuntimeException("Erro ao buscar os dados: ", e);
+        } finally{
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        return vagas;
+    }
+    public void delete(Vaga v)  {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement("DELETE  FROM vaga where idVaga =?");
+            stmt.setInt(1, v.getIdVaga());
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Vaga excluída com sucesso");
+        }
+        catch (SQLException e) {
+            throw new RuntimeException("Erro ao deletar vaga: " + e);
+        }
+        finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }    
+    }
+}
